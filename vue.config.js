025793d64df99a3cada061aa2b9e849b15d32e9a
chuck <121477964@qq.com>
2020-06-27 21:42:48 +0800

    将vue的本地的webpack的配置加上去了

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+/**
+ * vue的webpack配置
+ * 参考文档: https://cli.vuejs.org/zh/config/
+ *
+ */
+
 const path = require("path");
 
 function resolve(dir) {
@@ -5,6 +11,22 @@ function resolve(dir) {
 }
 
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/production-sub-path/" : "/",
+  outputDir: "dist",
+  devServer: {
+    proxy: {
+      // 设置本地调试域名的路径解决跨域问题
+      "/api": {
+        target: "<url>",
+        ws: true,
+        changeOrigin: true
+      },
+      "/foo": {
+        target: "<other_url>"
+      }
+    }
+  },
   chainWebpack: config => {
     config.resolve.alias
       .set("@", resolve("src"))
