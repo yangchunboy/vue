680398ccbb40393138e67dc0d47c685bcbf3e4bf
chuck <121477964@qq.com>
2020-07-16 22:52:43 +0800

    优化了样式了加了标题和logo

--- src/pages/speech/index.vue
+++ src/pages/speech/index.vue
@@ -1,43 +1,64 @@
 <template>
   <div class="speech">
+    <div class="title">
+      <h4>“助普杯”红河州“声动红河”朗诵大赛</h4>
+    </div>
     <div class="container">
       <Form>
-        <Field v-model="username"
-               required
-               name="用户名"
-               label="用户名"
-               placeholder="用户名" />
-        <Field readonly
-               clickable
-               required
-               name="picker"
-               :value="group"
-               label="报名组别"
-               placeholder="点击选择报名组别"
-               @click="showPicker = true" />
-        <Popup v-model="showPicker"
-               position="bottom">
-          <Picker show-toolbar
-                  :columns="columns"
-                  @confirm="onConfirm"
-                  @cancel="showPicker = false" />
+        <Field
+          v-model="username"
+          required
+          name="姓名"
+          label="姓名"
+          placeholder="请输入姓名"
+        />
+        <Field
+          v-model="idNumber"
+          required
+          name="身份证号"
+          label="身份证号"
+          placeholder="请输入身份证号"
+        />
+        <Field
+          readonly
+          clickable
+          required
+          name="picker"
+          :value="group"
+          label="报名组别"
+          placeholder="点击选择报名组别"
+          @click="showPicker = true"
+        />
+        <Popup v-model="showPicker" position="bottom">
+          <Picker
+            show-toolbar
+            :columns="columns"
+            @confirm="onConfirm"
+            @cancel="showPicker = false"
+          />
         </Popup>
-        <Field v-model="phone"
-               required
-               name="联系方式"
-               label="联系方式"
-               placeholder="请输入联系方式" />
-        <Field v-model="address"
-               required
-               name="联系地址"
-               label="联系地址"
-               placeholder="请输入联系地址" />
-        <Field v-model="email"
-               required
-               name="邮箱"
-               label="邮箱"
-               placeholder="请输入邮箱" />
-        <Field name="uploader"
+        <Field
+          v-model="phone"
+          required
+          name="联系方式"
+          label="联系方式"
+          placeholder="请输入联系方式"
+        />
+        <Field
+          v-model="address"
+          required
+          name="联系地址"
+          label="联系地址"
+          placeholder="请输入联系地址"
+        />
+        <Field
+          v-model="email"
+          required
+          name="邮箱"
+          label="邮箱"
+          placeholder="请输入邮箱"
+        />
+        <!-- <Field name="uploader"
                label="报名表照片"
                required>
           <template #input>
@@ -47,46 +68,53 @@
                       :after-read="afterSignPhotoRead"
                       :before-delete="deletePicture('signPhoto')" />
           </template>
-        </Field>
-        <Field name="uploader"
-               label="照片"
-               required>
+        </Field> -->
+        <Field name="uploader" label="参赛人照片" required>
           <template #input>
-            <Uploader v-model="photo"
-                      max-count="1"
-                      image-fit="fill"
-                      :after-read="afterPhotoRead"
-                      :before-delete="deletePicture('photo')" />
+            <Uploader
+              v-model="photo"
+              max-count="1"
+              image-fit="fill"
+              :after-read="afterPhotoRead"
+              :before-delete="deletePicture('photo')"
+            />
           </template>
         </Field>
-        <Field name="uploader"
-               label="音频文件"
-               required>
+        <Field name="uploader" label="音频文件" required>
           <template #input>
-
-            <div class="uploadFile">点击上传音频<input type="file"
-                     @change="audioChange" /></div>
+            <div class="uploadFile">
+              点击上传音频<input type="file" @change="audioChange" />
+            </div>
           </template>
         </Field>
-        <div style="margin: 16px;">
-          <Button round
-                  block
-                  type="info"
-                  native-type="submit"
-                  @click="submit">
+        <div class="submit">
+          <Button round block type="info" native-type="submit" @click="submit">
             提交
           </Button>
         </div>
       </Form>
     </div>
-    <div class="loading"
-         v-show="showLoading">
+    <div class="empty"></div>
+    <div class="logo">
+      <img src="../../assets/logo.jpeg" />
+      <div>助普科技</div>
+    </div>
+    <div class="loading" v-show="showLoading">
       <Loading color="#1989fa" />
     </div>
   </div>
 </template>
 <script>
-import { Form, Field, Uploader, Picker, Popup, Button, Toast, Loading } from "vant";
+import {
+  Form,
+  Field,
+  Uploader,
+  Picker,
+  Popup,
+  Button,
+  Toast,
+  Loading
+} from "vant";
 import api from "@api";
 export default {
   components: {
@@ -98,7 +126,7 @@ export default {
     Button,
     Loading
   },
-  data () {
+  data() {
     return {
       showPicker: false,
       username: "",
@@ -106,28 +134,28 @@ export default {
       group: "",
       address: "",
       email: "",
-      columns: ["专业组", "业余组", "少儿组"],
-      signPhoto: [],
+      idNumber: "",
+      columns: ["少儿组", "青年组", "成人组"],
       photo: [],
-      signPhotoUrl: "",
       photoUrl: "",
       audioUrl: "",
       showLoading: false
     };
   },
   methods: {
-    onConfirm (value) {
+    onConfirm(value) {
       this.showPicker = false;
       this.group = value;
     },
-    validate () {
+    validate() {
       const {
         username,
         phone,
         group,
         address,
         email,
-        signPhotoUrl,
+        idNumber,
+        // signPhotoUrl,
         photoUrl,
         audioUrl
       } = this;
@@ -136,8 +164,8 @@ export default {
         !phone.trim() ||
         !group ||
         !address ||
+        !idNumber.trim() ||
         !email ||
-        !signPhotoUrl ||
         !photoUrl ||
         !audioUrl
       ) {
@@ -158,8 +186,7 @@ export default {
       }
       return true;
     },
-    submit () {
-
+    submit() {
       const validateResult = this.validate();
       if (validateResult) {
         const {
@@ -168,7 +195,7 @@ export default {
           group,
           address,
           email,
-          signPhotoUrl,
+          idNumber,
           photoUrl,
           audioUrl
         } = this;
@@ -178,10 +205,10 @@ export default {
           group,
           address,
           email,
-          signPhotoUrl,
+          idNumber,
           photoUrl,
           audioUrl
-        }
+        };
         // 获取到所有数据后插入数据库
         // api.speech.insertSpeech(params).then((res) => {
         //   console.log(res);
@@ -190,42 +217,48 @@ export default {
         // })
       }
     },
-    generateFormData (file, callback) {
+    generateFormData(file, callback) {
       this.showLoading = true;
       const form = new FormData();
       form.append("file", file);
-      api.oss.uploadImg({ data: form, headers: { "Content-Type": "multipart/form-data" } }).then((res) => {
-        this.showLoading = false;
-        const { data } = res;
-        callback(data.data.url);
-      }).catch((error) => {
-        console.log(error);
-      })
+      api.oss
+        .uploadImg({
+          data: form,
+          headers: { "Content-Type": "multipart/form-data" }
+        })
+        .then(res => {
+          this.showLoading = false;
+          const { data } = res;
+          callback(data.data.url);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     },
-    deletePicture (type) {
-      if (type === "signPhoto") {
-        this.signPhotoUrl = "";
-      } else {
-        this.photoUrl = "";
-      }
+    deletePicture() {
+      this.photoUrl = "";
     },
-    afterSignPhotoRead (e) {
-      const file = e.file;
-      this.generateFormData(file, (url) => {
-        this.photoUrl = url;
-      })
-    },
-    audioChange (e) {
+    // afterSignPhotoRead (e) {
+    //   const file = e.file;
+    //   this.generateFormData(file, (url) => {
+    //     this.photoUrl = url;
+    //   })
+    // },
+    audioChange(e) {
       const file = e.target.files[0];
-      this.generateFormData(file, (url) => {
+      this.generateFormData(file, url => {
         this.audioUrl = url;
-      })
+      });
     },
-    afterPhotoRead (e) {
+    afterPhotoRead(e) {
       const file = e.file;
-      this.generateFormData(file, (url) => {
+      e.status = "uploading";
+      e.message = "上传中...";
+
+      this.generateFormData(file, url => {
         this.photoUrl = url;
-      })
+        e.status = "done";
+      });
     }
   }
 };
@@ -235,16 +268,26 @@ export default {
 .speech {
   width: 100%;
   height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  // overflow: hidden;
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
   background: @bgColor;
+  .title {
+    width: 100%;
+    height: 1rem;
+    font-size: 0.3rem;
+    text-align: center;
+    line-height: 1rem;
+    background: white;
+    margin-bottom: 0.2rem;
+  }
   .container {
     padding: 0.2rem;
     background: white;
     border-radius: 0.2rem;
     /deep/ .uploadFile {
-      width: 100%;
+      width: 60%;
       height: 0.6rem;
       text-align: center;
       line-height: 0.6rem;
@@ -257,6 +300,30 @@ export default {
         top: 0;
         opacity: 0;
       }
+    }
+    .submit {
+      width: 50%;
+      margin: 0.16rem auto;
+    }
+  }
+  .empty {
+    width: 100%;
+    height: 1.7rem;
+  }
+  .logo {
+    width: 100%;
+    height: 1.5rem;
+    background: white;
+    position: fixed;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    img {
+      width: 1rem;
+      height: 1rem;
+      margin-bottom: 0.1rem;
     }
   }
   .loading {
